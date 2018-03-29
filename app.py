d2f8a1a98786faa00502c9dcbdf2548ba2e53745
ABADIE Loïc <loic.abadie@pictime.com>
2018-03-29 15:04:02 +0200

    rajout schema

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sentinel import ResourceOwnerPasswordCredentials, oauth
-
+from flask_sentinel.data import Storage
+import json
 
 app = Flask(__name__)
 ResourceOwnerPasswordCredentials(app)
@@ -13,11 +14,17 @@ ResourceOwnerPasswordCredentials(app)
 def restricted_access():
     return "You made it through and accessed the protected resource!"
 
+@app.route('/userbycredential', methods=['POST'])
+def userbycredential():
+    if not request.json:
+        abort(400)
+
+    return json.dumps(request.json)
+    #Storage.save_user("***test", "password")
+
 @app.route('/')
 def home():
     return "hello"
 
 if __name__ == '__main__':
-    print "ResourceOwnerPasswordCredentials"
-    #ResourceOwnerPasswordCredentials(app)
-    #app.run(ssl_context='adhoc')
+    app.run(ssl_context='adhoc')
