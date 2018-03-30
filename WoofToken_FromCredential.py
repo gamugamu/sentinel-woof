fb1044f5601136d86af3deab5be0dece25b1a567
ABADIE Loïc <loic.abadie@pictime.com>
2018-03-30 15:28:07 +0200

    refactor

---
+++ WoofToken_FromCredential.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from flask_sentinel.data import Storage
+from flask import url_for
+import CredentialValidator as credential
+import hashlib
+import requests
+import UserHelper
+import json
+
+def credentialConversion(data):
+    # valide que le credential du provider est bon
+    code, user_cloud_info = credential.request_user_info_by_token(data["token"], data["provider"])
+    token           = {}
+    errorMessage    = ""
+
+    if code == 200:
+        #TODO user_info doit être similaire entre woofwoof, google, yahoo, et twitter
+        user_id     = user_cloud_info["id"]
+        user_pass   = hashlib.sha224(user_id).hexdigest()
+        # note: _user est privé. Ne pas exposer aux clients.
+        _user       = UserHelper.user_from_credential(user_id, user_pass)
+        r = requests.post(url_for('access_token', _external=True),
+                        data = {
+                            'client_id' : data["client_id"],
+                            'grant_type': 'password',
+                            'username'  : user_id,
+                            'password'  : user_pass})
+
+        token = json.loads(r.text)
+        # le client peut être invalide.
+        if "error" in token:
+            token = {}
+            errorMessage = 'cliend_id is invalid'
+        else:
+            #test
+            T = Storage.get_token(token["access_token"])
+            print "info: ", T, T.user, str(T.user._id)
+    # bad credential
+    else:
+        errorMessage = 'credential is invalid'
+
+    return token, errorMessage
--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request
 from flask_sentinel import ResourceOwnerPasswordCredentials, oauth
-from flask_sentinel.data import Storage
 import SchemaValidator as schema
-#TODO clean
-import CredentialValidator as credential
-import UserHelper
+import WoofToken_FromCredential as credConvertor
 import json
-import hashlib
-import requests
-
-from flask_sentinel.data import Storage
 
 app = Flask(__name__)
 ResourceOwnerPasswordCredentials(app)
@@ -30,37 +23,7 @@ def userbycredential():
     token = {}
 
     if isValid:
-        data = request.json
-        # valide que le credential du provider est bon
-        code, user_cloud_info = credential.request_user_info_by_token(data["token"], data["provider"])
-
-        if code == 200:
-            #TODO user_info doit être similaire entre woofwoof, google, yahoo, et twitter
-            user_id     = user_cloud_info["id"]
-            user_pass   = hashlib.sha224(user_id).hexdigest()
-            # note: _user est privé. Ne pas exposer aux clients.
-            _user       = UserHelper.user_from_credential(user_id, user_pass)
-            r           = requests.post("http://localhost:5000/oauth/token",
-                            data = {
-                                'client_id' : data["client_id"],
-                                'grant_type': 'password',
-                                'username'  : user_id,
-                                'password'  : user_pass})
-
-            token = json.loads(r.text)
-            # le client peut être invalide.
-            if "error" in token:
-                token = {}
-                errorMessage = 'cliend_id is invalid'
-
-            else:
-                #test
-                T = Storage.get_token(token["access_token"])
-                print "info: ", T, T.user, str(T.user._id)
-                
-        # bad credential
-        else:
-            errorMessage = 'credential is invalid'
+        token, errorMessage = credConvertor.credentialConversion(request.json)
 
     # retourne le compte
     return json.dumps({'token':token, 'error': errorMessage.__str__()}), code
