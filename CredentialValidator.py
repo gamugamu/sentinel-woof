2f0486b12369f2ae15ec755bed2fb279bf8b1eca
ABADIE Loïc <loic.abadie@pictime.com>
2018-04-03 09:32:58 +0200

    twitter test

--- CredentialValidator.py
+++ CredentialValidator.py
@@ -7,8 +7,29 @@ def request_user_info_by_token(token, validator):
     credentials = AccessTokenCredentials(token, 'user-agent-value')
     # Si c'est un compte local (woofwoof) alors il faut vérifier que la clès du compte existe
     # Si la clès n'existe pas, le compte n'existe pas
-
+    #request_user_info_twitter(credentials)
     return request_user_info_google(credentials)
+
+def request_user_info_twitter(credentials):
+    """
+    Info basic de twitter
+    """
+    http = httplib2.Http()
+    credentials.authorize(http)
+    print "FROM TWITTER"
+    try:
+        resp, content   = http.request('https://api.twitter.com/1.1/account/verify_credentials.json')
+        print "resp, content", resp, content
+
+        if resp.status != 200:
+            return resp.status, "Error while obtaining user profile: \n%s: %s" % resp, content
+
+        profil = json.loads(content.decode('utf-8'))
+        return resp.status, profil
+    # mauvais token
+    except Exception as e:
+        print "exc", e
+        return 400, e
 
 def request_user_info_google(credentials):
     """
