8bdc22b1b7fd6dec652c3b2a13dcaa4c59f83cc1
ABADIE Loïc <loic.abadie@pictime.com>
2018-04-05 14:55:17 +0200

    css - 2

--- static/doc.js
+++ static/doc.js
@@ -1,5 +1,5 @@
 Vue.component('step', {
-  props: ['title', 'method', 'url_root', 'route', 'use', 'bash_cmd', 'data', 'm_return', 'client_id', 'provider'],
+  props: ['title', 'method', 'url_root', 'route', 'use', 'curl_cmd', 'data', 'm_return'],
   template: `
     <div>
       </br>
@@ -20,31 +20,56 @@ Vue.component('step', {
        </tr>
        </tbody>
        </table>
-      <pre><code class="language-bash line-numbers">curl -i -d '{\"client_id\" : "{{client_id}}",  {{bash_cmd}} -H &quot;Content-Type: application/json&quot; -X {{method}} {{url_root}}{{route}}}</code></pre>
+      <pre><code class="language-bash line-numbers">{{curl_command}}</code></pre>
       <b>Return</b>
       <div class="grey lighten-2">
         <i><p>{{m_return}}</p></i>
       </div>
     </div>
-  `
-})
+  `,computed: {
+    // un accesseur (getter) calculé
+    curl_command: function () {
+      // `this` pointe sur l'instance vm
+      var cmd = "curl -i -H \"Content-Type: application/json\" -X " + this.method + " -d '{";
 
-//var bsh_me_auth = "\"provider\";:&quot;google&quot;, &quot;token&quot;:&quot;xxx&quot;}''"
+      for(var key in this.curl_cmd){
+        console.log("---> ", key, this.$parent._data[key]);
+        var value = this.curl_cmd[key]
+
+        if ( value.charAt(0) == '*' ) {
+          k = value.substr(1, value.lenght);
+          v = typeof this.$parent._data[k] !== 'undefined' ? this.$parent._data[k] : ""
+          
+          cmd += "\"" + key + "\":" + "\"" + v +  "\", "
+        }else{
+          cmd += "\"" + key + "\":" + "\"" + value +  "\", "
+        }
+      }
+      cmd = cmd.slice(0, -2); // ', '
+      cmd += "}' " + this.url_root + this.route;
+      return cmd
+    }
+  }
+})
 
 var app = new Vue({
   el: '#app',
   delimiters: ['${', '}'],
   data:{
-    client_id: sessionStorage.getItem("client_id")
+    client_id: sessionStorage.getItem("client_id"),
+    provider: sessionStorage.getItem("provider"),
+    auth_login: sessionStorage.getItem("auth_login"),
   },
   watch: {
         'client_id': function(val, oldVal){
-          console.log("called");
           sessionStorage.setItem("client_id", val)
         },
         'provider': function(val, oldVal){
-        console.log("p called");
-        sessionStorage.setItem("provider", val)
+          sessionStorage.setItem("provider", val)
+        },
+        'auth_login': function(val, oldVal){
+          console.log("****");
+          sessionStorage.setItem("auth_login", val)
         }
     }
 });
